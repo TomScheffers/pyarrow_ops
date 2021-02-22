3bee4410327e185950456723b70aa1fd3a4d450d
tom scheffers <tom@youngbulls.nl>
2021-02-22 23:01:56 +0100

    Fixed json array to table

--- pyarrow_ops/json.py
+++ pyarrow_ops/json.py
@@ -1,5 +1,8 @@
 import pyarrow as pa
+import json
+import numpy as np
 
-def arr_to_map(arr):
-    udt = pa.map_(pa.string(), pa.string())
-    return arr.cast(udt)
+def str_to_table(arr):
+    arr = arr.to_numpy()
+    arr = np.vectorize(json.loads)(arr)
+    return pa.Table.from_pydict({k: [dic.get(k, None) for dic in arr] for k in arr[0]})
