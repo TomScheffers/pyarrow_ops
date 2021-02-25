07a4d1095a797e856865eafb0ffe4fdbaddb417b
tom scheffers <tom@youngbulls.nl>
2021-02-25 16:21:00 +0100

    Added ML test example

--- test_ml.py
+++ test_ml.py
@@ -1,16 +1,35 @@
 import pyarrow as pa 
-from pyarrow_ops.ml import clean_num, clean_cat, clean_hot
+from pyarrow_ops import head, TableCleaner
 
-# Numericals
-arr = pa.array(["1", "2", None, "4"])
-print(clean_num(arr, impute=0, clip_min=2.))
+# Training data
+t1 = pa.Table.from_pydict({
+    'Animal': ['Falcon', 'Falcon', 'Parrot', 'Parrot', 'Parrot'],
+    'Max Speed': [380., 370., None, 26., 24.],
+    'Value': [2000, 1500, 10, 30, 20],
+})
 
-# Categoricals
-arr = pa.array(["A", "B", None, "C"])
-print(clean_cat(arr, categories=["A", "C", "B"]))
-print(clean_cat(arr))
+# Create TableCleaner
+cleaner = TableCleaner()
+cleaner.register_numeric('Max Speed', impute='min', clip=True)
+cleaner.register_label('Animal', categories=['Goose', 'Falcon']) # Categories is optional, unknown values get set to 0
+cleaner.register_one_hot('Animal')
 
-# One hots
-arr = pa.array(["A", "B", None, "C"])
-print(clean_hot(arr, categories=["A", "C", "B"]))
-print(clean_hot(arr))
+# Clean table and split into train/test
+X, y = cleaner.clean_table(t1, label='Value')
+head(X)
+X_train, X_test, y_train, y_test = cleaner.split(X, y)
+
+
+# Train a model + save cleaner dictionary for reuse (serialize to JSON or pickle)
+cleaner_dict = cleaner.to_dict()
+for c in cleaner_dict:
+    print(c)
+
+# Prediction data
+t2 = pa.Table.from_pydict({
+    'Animal': ['Falcon', 'Goose', 'Parrot', 'Parrot'],
+    'Max Speed': [380., 10., None, 26.]
+})
+new_cleaner = TableCleaner().from_dict(cleaner_dict)
+X_pred = new_cleaner.clean_table(t2)
+head(X_pred)
